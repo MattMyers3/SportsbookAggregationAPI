a53f444d5f9c087dd58bccfc31ebf4399a455d4b
Matthew Myers <mmyers12@villanova.edu>
2020-09-14 21:50:37 -0400

    PR feedback

--- SportsbookAggregationAPI/Controllers/GameLinesController.cs
+++ SportsbookAggregationAPI/Controllers/GameLinesController.cs
@@ -163,8 +163,7 @@ namespace SportsbookAggregationAPI.Controllers
         {
             var date = new DateTime(year, month, day);
             var gamesIds = context.GameRepository.Read().Where(r => r.TimeStamp.Date == date.Date).Select(g => g.GameId).ToList();
-            var gameLineRefreshes = context.GameLineRepository.Read().Where(r => gamesIds.Contains(r.GameId)).Select(l => l.LastRefresh);
-            return new LastRefresh { LastRefreshTime = gameLineRefreshes.Max() };
+            return new LastRefresh { LastRefreshTime = context.GameLineRepository.Read().Where(r => gamesIds.Contains(r.GameId)).Max(l => l.LastRefresh) };
         }
     }
 }
