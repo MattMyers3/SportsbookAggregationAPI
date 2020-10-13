ab28a608d927b1f5b9c6906e4350e0b77fbc40e8
Matthew Myers <mmyers12@villanova.edu>
2020-10-13 19:43:53 -0400

    Adding odds boosts api

---
+++ SportsbookAggregationAPI/Data/Configuration/OddsBoostConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SportsbookAggregationAPI.Data.Models;
+
+namespace SportsbookAggregationAPI.Data.Configuration
+{
+    public class OddsBoostConfiguration : IEntityTypeConfiguration<OddsBoost>
+    {
+        public void Configure(EntityTypeBuilder<OddsBoost> builder)
+        {
+            // default
+        }
+    }
+}
---
+++ SportsbookAggregationAPI/Data/Models/OddsBoost.cs
@@ -0,0 +1,21 @@
+﻿using SportsbookAggregation.Data.Models;
+using System;
+
+namespace SportsbookAggregationAPI.Data.Models
+{
+    public class OddsBoost
+    {
+        public Guid OddsBoostId { get; set; }
+        public string Description { get; set; }
+        public int PreviousOdds { get; set; }
+        public int BoostedOdds { get; set; }
+
+        public Sport Sport { get; set; }
+        public Guid SportId { get; set; }
+
+        public GamblingSite GamblingSite { get; set; }
+        public Guid GamblingSiteId { get; set; }
+
+        public bool IsAvailable { get; set; }
+    }
+}
