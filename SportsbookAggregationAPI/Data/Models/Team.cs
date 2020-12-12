f6aca6fa9c14982e71587d2fa991a38418db5d3e
NSmizzy <nismith@udel.edu>
2020-12-11 19:31:02 -0500

    added ncaab

--- SportsbookAggregationAPI/Data/Models/Team.cs
+++ SportsbookAggregationAPI/Data/Models/Team.cs
@@ -7,8 +7,5 @@ namespace SportsbookAggregation.Data.Models
         public Guid TeamId { get; set; }
         public string Mascot { get; set; }
         public string Location { get; set; }
-
-        public Sport Sport { get; set; }
-        public Guid SportId { get; set; }
     }
 }
